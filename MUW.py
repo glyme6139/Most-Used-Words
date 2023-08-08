631074d4d96ef3af16aa5d49022cbc92e951e29e
Ryan Ducret <ryanducret@live.fr>
2023-08-08 15:40:53 +0200

    Added Threshold, changed naming, moved gargbage functions to helper.py, removed useless files on git and added directory support

--- MUW.py
+++ MUW.py
@@ -5,95 +5,20 @@
 ######################
 
 # Imports
-import argparse
-import configparser
-import json
-import xml.etree.cElementTree as ET
+from helpers import *
 
 # Config Parsing
-default_number = 10
-stopwords = {}
-config = configparser.ConfigParser()
-config.read('config.conf')
-if "muw.settings" in config.sections():
-    default_number = config["muw.settings"].getint("DefaultNumber", 10)
-if "muw.stopwords" in config.sections():
-    for i in config["muw.stopwords"]:
-        stopwords[i] = str.lower(config["muw.stopwords"].get(i)).replace(
-            "[", "").replace("]", "").replace(" ", "").replace('"', "").split(",")
-else:
-    config["muw.settings"] = {"DefaultNumber": 10}
-    f = open("config.conf", "w")
-    config.write(f)
-    f.close()
+config = parse_config()
 
-
-# Argument Parsing
-parser = argparse.ArgumentParser(
-    prog='Most Used Words',
-    description='A very basic tool that let\'s you get the most used word in a text or file.',
-    epilog='Configuration is defined in config.conf,    MUW by Ryan Ducret')
-
-
-parser.add_argument('-f', '--file', help="Input file to read text from.",
-                    type=str)
-
-parser.add_argument('-i', '--input', help="Input text.",
-                    type=str)
-
-parser.add_argument('-o', '--output', help="Specifies the path to an output file.",
-                    type=str)
-
-parser.add_argument('-of', '--output-format', help="Specifies the format to use for the output. (default : json) (does not affect the extension)",
-                    type=str, default="json", choices=["json", "xml"])
-
-parser.add_argument('-n', '--number', help=f"Specifies the number of words to get the count of from the start argument. (default {default_number}, gets the {default_number} most used words) (if zero, shows count for every word)",
-                    type=int, default=default_number)
-
-parser.add_argument('-s', '--start', help=f"Specifies the starting number from wich to get the N^th most used words. (default 1, if 10 with n = 10 would show the 10th to 19th most used words)",
-                    type=int, default=1)
-
-parser.add_argument('-sw', '--stopwords', help="Specifies to remove supplied language specific stopwords (such as 'a' 'the' 'wich' etc.) (defined by languages in config.conf)",
-                    type=str, choices=list(stopwords.keys()))
-
-parser.add_argument('-t', '--threshold', help="Specifies the minimum number of word occurences at wich words will be added to output (default 0)",
-                    type=int, default=0)
-
-parser.add_argument('-v', '--verbose',
-                    action='store_true')
-
-
-args = parser.parse_args()
-
-
-# Argument Checks
-if args.start < 1:
-    raise ValueError(
-        f"Argument start supplied ({args.start}) is invalid, start must be at least 1.")
-
-if args.number < 0:
-    raise ValueError(
-        f"Number supplied ({args.number}) is invalid, number must be at least 0.")
-
-if args.threshold < 0:
-    raise ValueError(
-        f"Threshold supplied ({args.threshold}) is invalid, number must be at least 0.")
-
-if not args.file and not args.input:
-    raise SyntaxError(
-        f"You must supply an input either a file (-f <filenae>) or a text (-i <text>).")
-
-if args.file and args.input:
-    raise SyntaxError(
-        f"You must supply at most one input.")
-
+# Arguments Parsing
+args = parse_args()
 
 # Variables
-bVerbose = args.verbose
+total_word_count = None
+unique_word_count = None
+
 
 # Functions
-
-
 def count_words(word_list: list):
     word_dict = {}
     for w in word_list:
@@ -124,7 +49,7 @@ def remove_all_non_alpha(input_text: str):
     return ''.join([i for i in input_text if (i.isalpha() or i == " ")])
 
 
-def parse_text(text: str):
+def text_to_words(text: str):
     # Filters the text to remove all characters that are not letters or spaces and splits the text in words, converts tabs in spaces for splitting
     unprocessed_word_list = remove_all_non_alpha(
         text.strip().strip("\n").replace("\t", " ")).split(" ")
@@ -162,15 +87,14 @@ def save_results(filename, word_list):
         file_data = json.dumps(output_dict, indent=4)
 
     if args.output_format == "xml":
-        output_xml = ET.Element("data")
-        # populating xml tree
+        output_xml = "<data>\n"
         for w in word_list:
-            element = ET.SubElement(output_xml, "word")
-            ET.SubElement(element, "text").text = w[1]
-            ET.SubElement(element, "number").text = str(w[0])
-        # formating xml and dumping to string
-        ET.indent(output_xml)
-        file_data = ET.tostring(output_xml).decode("UTF-8")
+            output_xml += "    <word>\n"
+            output_xml += "        <text>"+w[1]+"</text>\n"
+            output_xml += "        <number>"+str(w[0])+"</number>\n"
+            output_xml += "    </word>\n"
+        output_xml += "</data>\n"
+        file_data = output_xml
 
     # writing data if any
     if file_data:
@@ -189,7 +113,8 @@ def filter_stopwords(word_list):
             filtered_word_list.append(w)
     return filtered_word_list
 
-def filter_threshold(word_list) :
+
+def filter_threshold(word_list):
     filtered_word_list = []
     for w in word_list:
         if w[0] >= args.threshold:
@@ -197,93 +122,89 @@ def filter_threshold(word_list) :
     return filtered_word_list
 
 
-if __name__ == "__main__":
+def process_input(input_text):
+    global total_word_count
+    global unique_word_count
+    
+    debug_print("Input Text", input_text)
 
-    input_text = ""
-    if args.file:
-        input_text = str.lower(read_file(args.file))
-    if args.input:
-        input_text = str.lower(args.input)
+    word_list = text_to_words(input_text)
 
-    if bVerbose:
-        print("\n\n-------- Input Text --------\n\n")
-        print(input_text)
-
-    word_list = parse_text(input_text)
-
-    if bVerbose:
-        print("\n\n-------- Parsed Text --------\n\n")
-        print(word_list)
+    debug_print("Text To Words", word_list)
+    total_word_count = len(word_list)
 
     word_count_dict = count_words(word_list)
 
-    if bVerbose:
-        print("\n\n-------- Word Count --------\n\n")
-        print(word_count_dict)
+    debug_print("Word Count", word_count_dict)
 
-    word_count = word_dict_to_word_list(word_count_dict)
+    unique_word_list = word_dict_to_word_list(word_count_dict)
 
-    if bVerbose:
-        print("\n\n-------- Word Dict To Word List --------\n\n")
-        print(word_count)
+    debug_print("Word Dict To Word List", unique_word_list)
+    unique_word_count = len(unique_word_list)
 
     if args.stopwords:
-        word_count = filter_stopwords(word_count)
+        unique_word_list = filter_stopwords(unique_word_list)
 
-        if bVerbose:
-            print("\n\n-------- Stopwords Filtered Word List --------\n\n")
-            print(word_count)
+        debug_print("Stopwords Filtered Word List", unique_word_list)
 
+    if args.threshold:
+        unique_word_list = filter_threshold(unique_word_list)
 
-    if args.threshold :
-        word_count = filter_threshold(word_count)
+        debug_print("Threshold Filtered Word List", unique_word_list)
 
-        if bVerbose:
-            print("\n\n-------- Threshold Filtered Word List --------\n\n")
-            print(word_count)
+    return unique_word_list
 
-
+def main(input_text,output_filename=None) :
     
+    unique_word_list = process_input(input_text)
 
-    if args.start > len(word_count):
+    # Last checks to ensure we don't access non existing memory
+
+    if args.start > len(unique_word_list):
         raise ValueError(
-            f"Argument start supplied ({args.start}) is bigger than the unique words count in the text ({len(word_count)}).")
+            f"Argument start supplied ({args.start}) is bigger than the unique words count in the text ({len(unique_word_list)}).")
 
-    if len(word_count) < 1:
-        exit()
+    if len(unique_word_list) < 1:
+        return
 
     # Text padding
     print("\n")
 
-    result_list = word_count
+    result_list = unique_word_list
 
     if args.number == 0:
-        for i in range(args.start-1, len(word_count)):
+        for i in range(args.start-1, len(unique_word_list)):
             print(
-                f'The {i+1}{get_number_suffix(i+1)} most used word in the text is : "{word_count[i][1]}" with {word_count[i][0]} occurences.')
+                f'The {i+1}{get_number_suffix(i+1)} most used word in the text is : "{unique_word_list[i][1]}" with {unique_word_list[i][0]} occurences.')
 
     else:
-        result_list = word_count[args.start -
-                                 1:min(args.start+args.number, len(word_count)+1)-1]
+        result_list = unique_word_list[args.start -
+                                       1:min(args.start+args.number, len(unique_word_list)+1)-1]
         for i in range(len(result_list)):
             print(
                 f'The {i+args.start}{get_number_suffix(i+args.start)} most used word in the text is : "{result_list[i][1]}" with {result_list[i][0]} occurences.')
 
-    # Text padding
-    print("\n")
-
-    print(f"({len(word_list)} words in text)")
-
-    # Text padding
-    print("\n")
+    print(
+        "\n"+f"({total_word_count} words in text, {unique_word_count} unique.)", end="\n\n")
 
     if args.output:
 
-        print(f"Saving results to {args.output}")
+        print(f"Saving results to {args.output}", end="\n\n")
 
-        # Text padding
-        print("\n")
+        print(save_results(args.output if not output_filename else output_filename, result_list))
 
-        print(save_results(args.output, result_list))
+    return
 
-    exit()
+
+
+if __name__ == "__main__":
+    if args.input :
+        main(str.lower(args.input))
+
+    elif args.file :
+        main(str.lower(read_file(args.file)))
+
+    elif args.directory :
+        for f in next(os.walk(args.directory))[2] :
+            main(str.lower(read_file(   os.path.abspath(args.directory+os.path.sep+f))),
+                                        os.path.abspath(args.output+os.path.sep+"muw_out_"+f.rsplit(".",1)[0]+f".{args.output_format}")) # passing output filename too, parsing f to remove extension of original file and appending new extension
---
+++ helpers.py
@@ -0,0 +1,136 @@
+
+import argparse
+import configparser
+import json
+import os
+
+default_number = 10
+default_start = 1
+default_format = "json"
+default_threshold = 1
+stopwords = {}
+verbose = False
+
+
+def debug_print(title, message):
+    if verbose:
+        print("\n\n"+f"{'-'*8} {title} {'-'*8}"+"\n\n")
+        print(message)
+
+
+def parse_config():
+    global default_number
+    global default_start
+    global default_format
+    global default_threshold
+    global stopwords
+    config = configparser.ConfigParser()
+    config.read('config.conf')
+    if "muw_settings" in config.sections():
+        default_number = config["muw_settings"].getint(
+            "default_number", default_number)
+        default_start = config["muw_settings"].getint(
+            "default_start", default_start)
+        default_format = config["muw_settings"].get(
+            "default_format", default_format)
+        default_threshold = config["muw_settings"].getint(
+            "default_threshold", default_threshold)
+    else:
+        config["muw_settings"] = {
+            "default_number": 10,
+            "default_start": 0,
+            "default_format": "json",
+            "default_threshold": 1
+        }
+        f = open("config.conf", "w")
+        config.write(f)
+        f.close()
+    if "muw_stopwords" in config.sections():
+        for i in config["muw_stopwords"]:
+            stopwords[i] = str.lower(config["muw_stopwords"].get(i)).replace(
+                "[", "").replace("]", "").replace(" ", "").replace('"', "").split(",")
+    return config
+
+# Variables
+
+
+def parse_args():
+    # Argument Parsing
+    parser = argparse.ArgumentParser(
+        prog='Most Used Words',
+        description='A very basic tool that let\'s you get the most used word in a text or file.',
+        epilog='Configuration is defined in config.conf,    MUW by Ryan Ducret')
+
+    parser.add_argument('-f', '--file', help="Input file to read text from.",
+                        type=str)
+
+    parser.add_argument('-d', '--directory', help="Processes every file in this directory independently.",
+                        type=str)
+
+    parser.add_argument('-i', '--input', help="Input text.",
+                        type=str)
+
+    parser.add_argument('-o', '--output', help="Specifies the path to an output file or directory if using -d.",
+                        type=str)
+
+    parser.add_argument('-of', '--output-format', help=f"Specifies the format to use for the output. (default : {default_format}) (does not affect the extension except if -d and -o are used)",
+                        type=str, default=default_format, choices=["json", "xml"])
+
+    parser.add_argument('-n', '--number', help=f"Specifies the number of words to get the count of from the start argument. (default {default_number}, gets the {default_number} most used words) (if zero, shows count for every word)",
+                        type=int, default=default_number)
+
+    parser.add_argument('-s', '--start', help=f"Specifies the starting number from wich to get the N^th most used words. (default {default_start}, if 10 with n = 10 would show the 10th to 19th most used words)",
+                        type=int, default=default_start)
+
+    parser.add_argument('-sw', '--stopwords', help="Specifies to remove supplied language specific stopwords (such as 'a' 'the' 'wich' etc.) (defined by languages in config.conf)",
+                        type=str, choices=list(stopwords.keys()))
+
+    parser.add_argument('-t', '--threshold', help=f"Specifies the minimum number of word occurences at wich words will be added to output (default {default_threshold})",
+                        type=int, default=default_threshold)
+
+    parser.add_argument('-v', '--verbose',
+                        action='store_true')
+
+    args = parser.parse_args()
+
+    # Argument Checks
+    if args.start < 1:
+        raise ValueError(
+            f"Argument start supplied ({args.start}) is invalid, start must be at least 1.")
+
+    if args.number < 0:
+        raise ValueError(
+            f"Number supplied ({args.number}) is invalid, number must be at least 0.")
+
+    if args.threshold < 1:
+        raise ValueError(
+            f"Threshold supplied ({args.threshold}) is invalid, number must be at least 1.")
+
+    if not args.file and not args.input and not args.directory:
+        raise SyntaxError(
+            f"You must supply an input either a file (-f <filenae>), a text (-i <text>), or a directory (-d <directory>).")
+
+    if args.file and args.input and args.directory:
+        raise SyntaxError(
+            f"You must supply at most one input.")
+    
+    if not os.path.exists(args.directory) :
+        raise RuntimeError(f"Directory {args.directory} not found.")
+    
+    print(list(os.walk(args.directory)))
+    if len(next(os.walk(args.directory))[2]) < 1 :
+        raise RuntimeError(f"Directory {args.directory} has no files.")
+    
+    if args.directory and args.output:
+        if os.path.exists(args.output) and os.path.isdir(args.output):
+            pass
+        elif os.access(os.path.dirname(args.output), os.W_OK):
+            os.mkdir(args.output)
+        else:
+            raise RuntimeError(f"Could not find output directory {args.output} and could not create it.")
+        
+
+    global verbose
+    verbose = args.verbose
+
+    return args
